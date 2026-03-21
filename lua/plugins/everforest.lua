e8c9669ce8ce48f837b311851c9fa268bfc93949
nxkh4ng <kh4ngnx@gmail.com>
2026-03-22 05:13:08 +0700

    style: formatting

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -1,20 +1,20 @@
 return {
-    "neanias/everforest-nvim",
-    version = false,
-    lazy = false,
-    priority = 1000,
-    opts = {
-        background = "hard",
-        ui_contrast = "high",
-        float_style = "bright",
-        transparent_background_level = 0,
-        italics = false,
-        disable_italic_comments = true,
-        dim_inactive_windows = true,
-    },
-    config = function(_, opts)
-        require("everforest").setup(opts)
-        vim.g.background = "dark"
-        vim.cmd([[colorscheme everforest]])
-    end,
+	"neanias/everforest-nvim",
+	version = false,
+	lazy = false,
+	priority = 1000,
+	opts = {
+		background = "hard",
+		ui_contrast = "high",
+		float_style = "bright",
+		transparent_background_level = 0,
+		italics = false,
+		disable_italic_comments = true,
+		dim_inactive_windows = true,
+	},
+	config = function(_, opts)
+		require("everforest").setup(opts)
+		vim.g.background = "dark"
+		vim.cmd([[colorscheme everforest]])
+	end,
 }
