dac89362e0a9ecb0529cf203698016c36744786c
nxkh4ng <kh4ngnx@gmail.com>
2026-03-22 04:14:03 +0700

    feat(plugin): add `nvim-lint` for linting
    Current using for JS, TS, Vue, Angular

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,21 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPost", "BufWritePost" },
+	config = function()
+		local lint = require("lint")
+
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			vue = { "eslint_d" },
+			angular = { "eslint_d" },
+		}
+
+		vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
+			group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
+}
